076e2c8b22dfa171d62f4818ac889f41ea8e2f14
Chen Liejian <chenlj@ihep.ac.cn>
2018-08-06 21:46:51 +0800

    gui added and cds

--- software/lib/core/include/JadeDataFrame.hh
+++ software/lib/core/include/JadeDataFrame.hh
@@ -3,14 +3,16 @@
 
 #include "JadeSystem.hh"
 
+#include <algorithm>
+#include <chrono>
+#include <functional>
 #include <string>
 #include <vector>
-#include <chrono>
 
-class DLLEXPORT JadeDataFrame{
- public:
-  JadeDataFrame(const std::string &data);
-  JadeDataFrame(std::string &&data);
+class DLLEXPORT JadeDataFrame {
+  public:
+  JadeDataFrame(const std::string& data);
+  JadeDataFrame(std::string&& data);
   JadeDataFrame(size_t nraw);
   JadeDataFrame() = delete;
   virtual ~JadeDataFrame();
@@ -21,6 +23,7 @@ class DLLEXPORT JadeDataFrame{
   std::chrono::system_clock::time_point& TimeStamp();
   bool IsInMatrix(size_t x, size_t y) const;
   int16_t GetHitValue(size_t x, size_t y) const;
+  int16_t GetCDSValue(size_t x, size_t y) const;
   std::vector<int16_t> GetFrameData() const;
   uint32_t GetMatrixLowX() const;
   uint32_t GetMatrixLowY() const;
@@ -28,9 +31,15 @@ class DLLEXPORT JadeDataFrame{
   uint32_t GetMatrixHighY() const;
   uint32_t GetMatrixSizeX() const;
   uint32_t GetMatrixSizeY() const;
+  uint32_t GetTriggerSerialOrder() const;
   void Print(std::ostream& os, size_t ws = 0) const;
- private:
+  std::vector<int16_t> GetFrameCDS();
+  bool GetCDSStatus();
+  void CDS(JadeDataFrame& df);
+
+  private:
   bool m_is_decoded;
+  bool m_is_cds;
   std::string m_data_raw;
   std::string m_description;
   std::chrono::system_clock::time_point m_ts;
@@ -42,6 +51,7 @@ class DLLEXPORT JadeDataFrame{
   uint16_t m_n_x;
   uint16_t m_n_y;
   std::vector<int16_t> m_data;
+  std::vector<int16_t> m_cds_frame_adc;
 };
 
 using JadeDataFrameSP = std::shared_ptr<JadeDataFrame>;
